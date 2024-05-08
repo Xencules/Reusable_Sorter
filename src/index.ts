897dd4e4313e2cc440c713d8e718d9e23c16f20f
Xencules <oskari.ahoniemi@gmail.com>
2024-05-08 12:20:12 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { Sorter } from "./Sorter/Sorter";
+import { NumbersCollection } from "./Sorter/Collections/NumbersCollection";
+import { CharactersCollection } from "./Sorter/Collections/CharactersCollection";
+import { LinkedList } from "./Sorter/Collections/LinkedList";
+
+//NUMBER ARRAY SORTER
+/* 
+const numbersCollection = new NumbersCollection([1,55,-54,222]);
+numbersCollection.sort();
+console.log(numbersCollection.data); */
+
+
+//STRING SORTER
+/* 
+const charactersCollection = new CharactersCollection('XavCgfj');
+charactersCollection.sort();
+console.log(charactersCollection.data); */
+
+
+// LinkedList Sorter
+
+/* const linkedList = new LinkedList();
+linkedList.add(50);
+linkedList.add(-120);
+linkedList.add(-167);
+linkedList.add(54);
+linkedList.add(3);
+linkedList.add(12);
+linkedList.add(-32);
+linkedList.sort();
+linkedList.print(); */
